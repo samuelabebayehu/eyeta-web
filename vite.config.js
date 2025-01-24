aab8593c0d64eec4e16746c00a241ec57046ad89
samuelabebayehu <saa2280@cumc.columbia.edu>
2025-01-24 16:21:44 +0300

    started update

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig(() => ({
   server: {
     port: 3000,
     proxy: {
-      '/api/socket': 'ws://localhost:8082',
-      '/api': 'http://localhost:8082',
+      '/api/socket': 'ws://demo4.traccar.org:8082',
+      '/api': 'http://demo4.traccar.org:8082',
     },
   },
   build: {
